14dd9688d7b713fd45d2f14b06cd668f78761388
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-02-14 02:33:56 +0200

    allow passing a file name

--- ct.js
+++ ct.js
@@ -2,7 +2,12 @@ const fs = require("fs");
 const { evaluateMany } = require("./evaluator.js");
 const { parseProgram } = require("./parser");
 
-const program = fs.readFileSync("example.ct", "utf8");
+const fileName = process.argv[2];
+if (!fileName) {
+  throw new Error("Please pass a file name");
+}
+
+const program = fs.readFileSync(fileName, "utf8");
 const parsedProgram = parseProgram(program);
 
 const prelude = fs.readFileSync("prelude.ct", "utf8");
